bcae025b201b8a5b3a37ea8da5b11c13b06869c6
Reeves Farrell <reevesfarrell13@gmail.com>
2025-12-29 13:37:56 -0500

    Fix makefile linking for SDL and get basic black window running

--- Makefile
+++ Makefile
@@ -4,8 +4,14 @@
 # Compiler
 CXX = g++
 
+#SDL path
+SDL_PATH = C:/sdl3
+
 # Compiler flags
-CXXFLAGS = -Wall -g
+CXXFLAGS = -Wall -g -I$(SDL_PATH)/include
+
+# Linker flags
+LDFLAGS = -L$(SDL_PATH)/lib -lmingw32 -lSDL3 -mwindows
 
 # Target executable
 TARGET = pong
@@ -19,16 +25,23 @@ SOURCES = main.cpp
 # Object files
 OBJECTS = $(SOURCES:.cpp=.o)
 
+# DLL path
+DLL_SRC = C:\sdl3\bin\SDL3.dll
+
 # Default rule to build and run the executable
 all: $(TARGET) run
 
 # Rule to link object files into the target executable
 $(TARGET): $(OBJECTS)
-	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJECTS)
+	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJECTS) $(LDFLAGS)
 
 # Rule to compile .cpp files into .o files
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+# Copy the DLL to the project root so that the program can run
+copy_dll:
+	@if exist $(DLL_SRC) (copy /Y $(DLL_SRC) . >nul) else (echo Warning: SDL3.dll not found!)
 
 # Rule to run the executable
 run: $(TARGET)
@@ -36,4 +49,4 @@ run: $(TARGET)
 
 # Clean rule to remove generated files
 clean:
-	del $(TARGET_DEL) $(OBJS)
+	del $(TARGET_DEL) $(OBJECTSS) SDL3.dll 2>nul
--- main.cpp
+++ main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
-#include "SDL3/SDL.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 using namespace std;
 
-int main() {
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
-    cout << "Hello World!" << endl;
+int main(int argc, char* argv[]) {
+
+    // Initialize SDL components
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Creates the game window
+    SDL_Window* window = SDL_CreateWindow("Pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+
+    // Creates the renderer for the game window
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    bool running = true;
+
+    // Game logic
+    while (running) {
+
+        SDL_Event event;
+
+        // Process events in the window until the user exits the program
+        while (SDL_PollEvent(&event)) {
+
+            if (event.key.key == SDLK_ESCAPE || event.type == SDL_EVENT_QUIT) {
+
+                running = false;
+            }
+        }
+
+        // Clear the window to black
+        // R,G,B
+        SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // Rendering will occur here
+        
+        // Presents the backbuffer
+        SDL_RenderPresent(renderer);
+    }
+    
+    // Cleanup SDL 
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
