33fdd209683dca2c1e49d09d2f8a382517ed25cf
Reeves Farrell <reevesfarrell13@gmail.com>
2025-12-30 16:47:32 -0500

    Comment cleanup

--- score.cpp
+++ score.cpp
@@ -7,10 +7,11 @@ SDL_Texture* CreateScoreTexture(SDL_Renderer* renderer, TTF_Font* font, int sc1,
     snprintf(scoreBuf, sizeof(scoreBuf), "%d : %d", sc1, sc2);
     
     SDL_Color white = {255, 255, 255, 255};
-    // Use TTF_RenderText_Blended for much smoother, anti-aliased numbers
+
     SDL_Surface* surface = TTF_RenderText_Blended(font, scoreBuf, 0, white);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     
-    SDL_DestroySurface(surface); // SDL3 uses SDL_DestroySurface instead of FreeSurface
+    SDL_DestroySurface(surface);
+    
     return texture;
 }
